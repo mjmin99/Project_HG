ca5020ad260bb611df32cef4a2f8c924b25a26e9
mjmin99 <mjmin99@naver.com>
2025-12-03 17:07:06 +0900

    [Chore] 스파인 유니티, 파이어베이스 추가
    스파인 유니티, 파이어베이스 추가

---
+++ Assets/BackendManager.cs
@@ -0,0 +1,46 @@
+﻿using Firebase;
+using Firebase.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackendManager : MonoBehaviour
+{
+    public static BackendManager Instance { get; private set; }
+
+    private FirebaseApp app;
+    public static FirebaseApp App => Instance.app;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.Result == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                app = FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Debug.Log("Firebase dependencies check success");
+            }
+            else
+            {
+                Debug.LogError($"Could not resolve all Firebase dependencies: {task.Result}");
+                // Firebase Unity SDK is not safe to use here.
+                app = null;
+            }
+        });
+    }
+}
