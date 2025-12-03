1197d09c77327014b3dfdb82b4fcbc6a8e50d8cb
mjmin99 <mjmin99@naver.com>
2025-12-03 20:02:00 +0900

    [Test] 파이어베이스 연동 테스트
    문제없이 잘 됨

--- Assets/1. Test/FirebaseManager.cs
+++ Assets/1. Test/FirebaseManager.cs
@@ -1,8 +1,28 @@
 ﻿using UnityEngine;
 using Firebase.Extensions;
+using Firebase;
 
 public class FirebaseManager : MonoBehaviour
 {
+    private static FirebaseManager instance;
+    public static FirebaseManager Instance { get { return instance; } }
+
+    private static FirebaseApp app;
+    public static FirebaseApp App { get { return app; } }
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        { 
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
         Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
@@ -10,10 +30,12 @@ public class FirebaseManager : MonoBehaviour
             if (dependencyStatus == Firebase.DependencyStatus.Available)
             {
                 Debug.Log("파이어베이스 설정이 모두 충족되어 사용할 수 있는 상황");
+                app = FirebaseApp.DefaultInstance;
             }
             else
             {
                 Debug.LogError($"파이어 베이스 설정이 충족되지 않아 실패했습니다. 이유 : {dependencyStatus}");
+                app = null;
             }
         });
     }
