324f21cb14c411893249aab26d5fd36d3131a526
mjmin99 <mjmin99@naver.com>
2025-12-03 19:23:28 +0900

    [Chore] 유니rx, 유니task 설치

---
+++ Assets/1. Test/FirebaseManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Firebase.Extensions;
+
+public class FirebaseManager : MonoBehaviour
+{
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                Debug.Log("파이어베이스 설정이 모두 충족되어 사용할 수 있는 상황");
+            }
+            else
+            {
+                Debug.LogError($"파이어 베이스 설정이 충족되지 않아 실패했습니다. 이유 : {dependencyStatus}");
+            }
+        });
+    }
+}
