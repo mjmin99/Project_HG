48437a9b84cfab2b7eb56ac15ecabea738a9cc13
mjmin99 <mjmin99@naver.com>
2025-12-24 13:25:19 +0900

    [Feat] 옵션 패널 구현

---
+++ Assets/MMJ/OptionPanel/OptionPanel.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class OptionPanel : UIPanel
+{
+    [Header("Buttons")]
+    [SerializeField] private Button btnClose;
+
+    [Header("Tabs")]
+    [SerializeField] private Button btnSound;
+    [SerializeField] private Button btnHowToPlay;
+
+    [Header("Tab Contents")]
+    [SerializeField] private GameObject soundTab;
+    [SerializeField] private GameObject howToPlayTab;
+
+    private GameObject currentTab;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        btnClose.onClick.AddListener(() =>
+        {
+            UIManager.Instance.CloseTop();
+        });
+
+        btnSound.onClick.AddListener(() =>
+        {
+            OpenTab(soundTab);
+        });
+
+        btnHowToPlay.onClick.AddListener(() =>
+        {
+            OpenTab(howToPlayTab);
+        });
+    }
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+        OpenTab(soundTab); // 기본 진입 탭
+    }
+
+    private void OpenTab(GameObject tab)
+    {
+        if (currentTab == tab) return;
+
+        if (currentTab != null)
+            currentTab.SetActive(false);
+
+        currentTab = tab;
+        currentTab.SetActive(true);
+    }
+}
