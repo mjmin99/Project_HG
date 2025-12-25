976d1bc839064dbbb54ce3c57f04ad02e5028b31
mjmin99 <mjmin99@naver.com>
2025-12-25 19:31:40 +0900

    [Chore] 옵션ui 수정

---
+++ Assets/MMJ/New Stage/StageRuntimeManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class StageRuntimeManager : MonoBehaviour
+{
+    public static StageRuntimeManager Instance { get; private set; }
+
+    public StageDataSO CurrentStage { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetStage(StageDataSO stage)
+    {
+        CurrentStage = stage;
+    }
+
+    public void Clear()
+    {
+        CurrentStage = null;
+    }
+}
